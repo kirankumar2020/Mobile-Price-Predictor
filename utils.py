05d1303643f983227365973fce575f08a683d2dd
kirankumar2020 <kale.kk2020@gmail.com>
2024-03-01 15:59:26 +0530

    update

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_predicted_price_class(battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep,mobile_wt,n_cores,pc,px_height,px_width,ram,sc_h,sc_w,talk_time,three_g,touch_screen,wifi):
-    mobile_path = r"D:\PDS\Flask\Mobile_phone\MobilePhone....KNN_clf_model.pkl"
+    mobile_path = r"MobilePhone....KNN_clf_model.pkl"
     
     with open(mobile_path, 'rb') as f:
         model = pickle.load(f)
